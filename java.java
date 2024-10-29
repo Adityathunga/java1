c5d54772d31a9cbf656eed1f867a2a9ea6780b7a
Adityathunga <aditya.23cs004@sode-edu.in>
2024-10-29 15:03:33 +0530

    Program8

--- java.java
+++ java.java
@@ -3,12 +3,34 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Fahrenheit;
 
 /**
  *
  * @author smv
  */
-public class java {
+class Outer{
+    void display(){
+        System.out.println("outer class display method");
+        
+    }
+    class Inner{
+        void display(){
+            System.out.println("inner class display method");
+            
+        }
+    }
+}
+public class program8 {
+    public static void main(String[] args){
+        String title0="lab 8 program=Nested Class";
+        String title1="Devlop a java program to create an outer class with afunction display";
+        String title2="Call another class inside the outer class named inner with a function called display";
+        String title3="Call thethethe two functions in thethethe main class";
+        System.out.println(title0+"\n"+title1+"\n"+title2+"\n"+title3+"\n");
+        Outer o1=new Outer();
+        o1.display();
+        Outer.Inner i1=o1.new Inner();
+        i1.display();
+    }
     
 }
