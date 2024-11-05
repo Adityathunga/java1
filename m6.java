908b161d729a287f6a7ab03a594e50028b59756a
Adityathunga <aditya.23cs004@sode-edu.in>
2024-11-05 16:14:13 +0530

    Add files via upload

---
+++ m6.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg1;
+class Tools1{
+	String s1="";
+	String s2="";
+	int len1=0;
+	public String reverse(String s1) {
+		len1=s1.length();
+		for(int i=0;i<len1;i++) {
+			s2=s2+s1.substring(len1-(i+1),len1-i);
+		}
+		return s2;
+	}
+}
+
+/**
+ *
+ * @author smv
+ */
+public class m6 {
+    public static void main(String[] args){
+        Tools1 t1=new Tools1();
+        System.out.println(t1.reverse("Madan"));
+    }
+    
+}
